6b70a0c46c28e0a34bdc1d63c01699e424119225
Ashikur Rahman Bhuiyan <ashik1291@users.noreply.github.com>
2018-03-08 13:47:18 +0600

    Create URI 1759.cpp

---
+++ URI 1759.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        if(i==n-1) printf("Ho!");
+        else printf("Ho ");
+    }
+    printf("\n");
+
+    return 0;
+}
